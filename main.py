1ffb1ba354cef663c53eb1b6d6d1c0532fe782a4
Daniele Scasciafratte <mte90net@gmail.com>
2022-04-04 10:52:17 +0200

    feat(bot): allineato senza la parte proxy

--- main.py
+++ main.py
@@ -183,7 +183,6 @@ class PlaceClient:
             url,
             headers=headers,
             data=payload,
-            proxies=self.GetRandomProxy(self),
         )
         logger.debug("Thread #{} : Received response: {}", thread_index, response.text)
 
@@ -330,7 +329,6 @@ class PlaceClient:
                                         requests.get(
                                             msg["data"]["name"],
                                             stream=True,
-                                            proxies=self.GetRandomProxy(self),
                                         ).content
                                     )
                                 ),
@@ -536,7 +534,6 @@ class PlaceClient:
                     while True:
                         try:
                             client = requests.Session()
-                            client.proxies = self.GetRandomProxy(self)
                             client.headers.update(
                                 {
                                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36"
@@ -557,7 +554,6 @@ class PlaceClient:
                             r = client.post(
                                 "https://www.reddit.com/login",
                                 data=data,
-                                proxies=self.GetRandomProxy(self),
                             )
                             break
                         except Exception:
